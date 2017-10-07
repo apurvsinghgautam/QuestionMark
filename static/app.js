4f51b1450aa0acc6ce7eb0e4bedbef6549b8935e
Rohan Nevrikar <rohan.nevrikar@gmail.com>
2017-10-07 18:42:13 +0530

    Add files via upload
    JS files

---
+++ static/app.js
@@ -0,0 +1,32 @@
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    
+        request.execute(function (resp) {
+          console.log(profile.getId());
+          console.log('Name: ' + profile.getName());
+          console.log('Image URL: ' + profile.getImageUrl());
+          console.log('Email: ' + profile.getEmail());
+        });
+
+}
+function onFailure(error) {
+    alert(error);
+}
+function renderButton() {
+    gapi.signin2.render('gSignIn', {
+        'scope': 'profile email',
+        'width': 240,
+        'height': 50,
+        'longtitle': true,
+        'theme': 'dark',
+        'onsuccess': onSuccess,
+        'onfailure': onFailure
+    });
+}
+
+	function signOut() {
+	    var auth2 = gapi.auth2.getAuthInstance();
+	    auth2.signOut().then(function () {
+	      console.log('User signed out.');
+	    });
+	  }
