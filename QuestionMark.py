3007dfa0762c9b7c8977722bce090596e2c35248
Apurv Singh Gautam <apurvsinghgautam@gmail.com>
2017-10-15 18:56:53 +0530

    Updated css & js file paths
    Signed-off-by: Apurv Singh Gautam <apurvsinghgautam@gmail.com>

--- QuestionMark.py
+++ QuestionMark.py
@@ -1,17 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('index.html')
 
 
 @app.route('/profile')
 def profile():
-    return 'Hi there'
+    return render_template('profile.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='localhost', port=4444)
