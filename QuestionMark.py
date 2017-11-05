a25f2caf4ad6f962cb5fdc660faa06399bb48d94
Apurv Singh Gautam <apurvsinghgautam@gmail.com>
2017-11-05 18:31:28 +0530

    Login & Register Functionality Added
    Signed-off-by: Apurv Singh Gautam <apurvsinghgautam@gmail.com>

--- QuestionMark.py
+++ QuestionMark.py
@@ -18,7 +18,7 @@ mysql = MySQL(app)
 
 
 @app.route('/')
-def hello_world():
+def index():
     return render_template('index.html')
 
 
@@ -27,13 +27,89 @@ def profile():
     return render_template('profile.html')
 
 
+# Register Form Class
+class RegisterForm(Form):
+    username = StringField('Username', [validators.Length(min=4, max=50)])
+    email = StringField('Email', [validators.Length(min=6, max=100)])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords do not match')
+    ])
+    confirm = PasswordField('Confirm Password')
 
 
+# User Register
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        email = form.email.data
+        username = form.username.data
+        password = sha256_crypt.encrypt(str(form.password.data))
+
+        # Create cursor
+        cur = mysql.connection.cursor()
+
+        # Execute query
+        cur.execute("INSERT INTO UserProfile(Email, Uname, Upass) VALUES(%s, %s, %s)", (email, username, password))
+
+        # Commit to DB
+        mysql.connection.commit()
+
+        # Close connection
+        cur.close()
+
+        flash('You are now registered and can log in', 'success')
+
+        return redirect(url_for('login'))
+    return render_template('auth/register.html', form=form)
+
+
+# User login
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # Get Form Fields
+        username = request.form['username']
+        password_candidate = request.form['password']
+
+        # Create cursor
+        cur = mysql.connection.cursor()
+
+        # Get user by username
+        result = cur.execute("SELECT * FROM UserProfile WHERE Uname = %s", [username])
+
+        if result > 0:
+            # Get stored hash
+            data = cur.fetchone()
+            password = data['password']
+            pro_id = data['ProID']
+
+            # Compare Passwords
+            if sha256_crypt.verify(password_candidate, password):
+                # Passed
+                session['logged_in'] = True
+                session['username'] = username
+                session['pro_id'] = pro_id
+
+                flash('You are now logged in', 'success')
+                return redirect(url_for('profile'))
+            else:
+                error = 'Invalid login'
+                return render_template('auth/login.html', error=error)
+            # Close connection
+            cur.close()
+        else:
+            error = 'Username not found'
+            return render_template('auth/login.html', error=error)
+
+    return render_template('auth/login.html')
 
 
 # Answer Form Class
 class AnswerForm(Form):
     body = TextAreaField('AnsBody', [validators.Length(min=30)])
+
 
 # Add Answer
 @app.route('/add_answer', methods=['GET', 'POST'])
@@ -51,7 +127,7 @@ def add_answer():
         # Commit to DB
         mysql.connection.commit()
 
-        #Close connection
+        # Close connection
         cur.close()
 
         flash('Answer Posted', 'success')
@@ -60,5 +136,8 @@ def add_answer():
 
     return render_template('add_answer.html', form=form)
 
+
 if __name__ == '__main__':
+    app.secret_key = 'super secret key'
+    app.config['SESSION_TYPE'] = 'filesystem'
     app.run(host='localhost', port=4444, debug=True)
